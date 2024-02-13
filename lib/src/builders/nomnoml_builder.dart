3e6dd9347ad7a42d39c31aa9e2418778b028f592
Robbert Halff <rob.halff@gmail.com>
2024-02-13 23:36:07 +0100

    Cast InterfaceType to ClassElement.

--- lib/src/builders/nomnoml_builder.dart
+++ lib/src/builders/nomnoml_builder.dart
@@ -35,7 +35,7 @@ class NomnomlBuilder implements DiagramBuilder {
 
   @override
   void addInterface(InterfaceType element) {
-    final interfaceName = fullClassName(element.element);
+    final interfaceName = fullClassName(element.element as ClassElement);
     _relationships.add('[$interfaceName]<:--[$_currentClass]');
   }
 
@@ -46,13 +46,13 @@ class NomnomlBuilder implements DiagramBuilder {
 
   @override
   void addMixin(InterfaceType element) {
-    final mixinName = fullClassName(element.element);
+    final mixinName = fullClassName(element.element as ClassElement);
     _relationships.add('[$mixinName]<:-[$_currentClass]');
   }
 
   @override
   void addSuper(InterfaceType element) {
-    final superName = fullClassName(element.element);
+    final superName = fullClassName(element.element as ClassElement);
     _relationships.add('[$superName]<:-[$_currentClass]');
   }
 
